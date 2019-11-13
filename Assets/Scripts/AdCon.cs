5184598219e393d80b0779573c362c23a6c5af01
Martin <mar.sekeres@gmail.com>
2019-11-13 15:15:57 +0000

    Completed Project

---
+++ Assets/Scripts/AdCon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Advertisements;
+public class AdCon : MonoBehaviour
+{
+    bool adShown = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Advertisement.IsReady() && adShown == false) {
+            Debug.Log("Ad is ready");
+            Advertisement.Show();
+            adShown = true;
+        }
+    }
+}
---
+++ Assets/Scripts/BController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Advertisements;
+public class BController : MonoBehaviour
+{
+    bool adShown = false;
+    public void ButtonClick(string sceneName) {
+        if (adShown == true) {
+            Application.LoadLevel(sceneName);
+        } else if (Advertisement.IsReady() && adShown == false) {
+            Debug.Log("Ad is ready");
+            Advertisement.Show();
+            Debug.Log("Ad shown");
+            adShown = true;
+        }
+    }
+    //Under Main Menu
+    public void playButton(string sceneName) {
+        Application.LoadLevel(sceneName);
+    }
+    
+    public void settingsButton(string sceneName){
+        Application.LoadLevel(sceneName);
+    }
+    //Under Settigns Menu
+    public void returnButton(string sceneName){
+        Application.LoadLevel(sceneName);
+    }
+    public void exitButton(){
+        //Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+    public void returnFromSceneButton(string sceneName){
+        Application.LoadLevel(sceneName);
+    }
+}
